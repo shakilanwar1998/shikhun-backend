0f5333259d20b235f2c2d27b7a9a70e0df3e8f9c
AminHossain <aminhossain283@gmail.com>
2021-05-16 11:41:45 +0600

    migration files added

--- database/migrations/2017_08_11_073824_create_menus_wp_table.php
+++ database/migrations/2017_08_11_073824_create_menus_wp_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateMenusWpTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create( config('menu.table_prefix') . config('menu.table_name_menus'), function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +27,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists( config('menu.table_prefix') . config('menu.table_name_menus'));
     }
 }
