76186b3bcd20ca0e7c6560249967a7e692dbd418
Shakil Anwar <shakilanwar1998@gmail.com>
2021-05-18 13:59:56 +0600

    Migrations updated

--- app/User.php
+++ app/User.php
@@ -15,9 +15,9 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
+    protected $fillable = array(
         'name', 'email', 'password',
-    ];
+    );
 
     /**
      * The attributes that should be hidden for arrays.
---
+++ config/access.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'table_names' => [
+        'users' => 'users'
+    ]
+];
